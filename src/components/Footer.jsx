c4ac059c5d0e3d7a76cacaac1dca48ed022b0361
KvJellyBean <kevinnatan91@gmail.com>
2024-04-09 16:29:16 +0700

    feat: add Footer component
    This commit add a footer to the website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; 2024 <a href="https://github.com/KvJellyBean">KvJellyBean</a>.
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
