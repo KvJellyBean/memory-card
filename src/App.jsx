f68e812f3c82004c20e2952500b7ed89bded047c
KvJellyBean <kevinnatan91@gmail.com>
2024-05-01 15:23:46 +0700

    feat: add function to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from "react";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pokemon, setPokemon] = useState();
+  const url = "https://pokeapi.co/api/v2/pokemon";
+
+  // Fetch data for pokemon from Poke api
+  const fetchData = async () => {
+    let newPokemonData = [];
+    const data = await fetch(url);
+    const parsedData = await data.json();
+
+    parsedData.results.forEach(async (result) => {
+      const pokemonData = await fetchPokemonData(result);
+      newPokemonData.push({
+        id: pokemonData.id,
+        name: pokemonData.name,
+        image: pokemonData.sprites.front_default,
+      });
+    });
+
+    setPokemon(newPokemonData);
+  };
+
+  // Fetch the specific pokemon's data
+  const fetchPokemonData = async (data) => {
+    const pokemonData = await fetch(data.url);
+    return await pokemonData.json();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <h1>PokeMatch</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header></Header>
+      <MainPage pokemon={pokemon}></MainPage>
+      <Footer></Footer>
     </>
   );
 }
